6577dfa761dea935ca8c56fc1b4f0ad6156e8874
John Doe <johndoe@example.com>
2022-05-02 18:52:26 +0200

    IT3 not working

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  
+  
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+
+
+  const subT = Number(price * quantity);
+
+  const subTotalHolder = product.querySelector('.subtotal span')
+  subTotalHolder.innerHTML = subT;
+  return subT;
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  const allProducts = document.getElementsByClassName('.product');
   // ITERATION 3
-  //... your code goes here
+  let total = 0;
+  for (let product of allProducts) {
+    total += updateSubtotal(product);
+  }
+  
+  const totalValueHolder = document.querySelector('#total-value span');
+  totalValueHolder.innerHTML = total;
 }
-
+// calculateAll(updateSubtotal())
 // ITERATION 4
 
 function removeProduct(event) {
